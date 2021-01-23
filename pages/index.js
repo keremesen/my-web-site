5191b3da3e27629a399fca6ff7d00987f42953aa
Kerem Esen <kerem.esen.01@hotmail.com>
2021-01-23 16:57:18 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,129 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Box, Flex, Heading, Text, Link, Grid } from "@chakra-ui/react";
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaGithub,
+  FaTwitter,
+  FaMailchimp,
+  FaMailBulk,
+} from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Flex align="center" justify="center" height="100vh" bg="#111">
+      <Flex
+        direction="column"
+        bg="#2222"
+        w="1080px"
+        height="60vh"
+        color="white"
+        boxShadow="2xl"
+        borderRadius={15}
+      >
+        <Flex direction="column" m={8}>
+          <Heading>KEREM ESEN </Heading>
+          <Text fontSize="xl"> FULL-STACK DEVELOPER</Text>
+        </Flex>
+        <Flex bg="#3333" h="30%" align="center" justify="center" my={5}>
+          <Heading size="md">
+            hi im kerem im 19 yo developer.im trying to learn and do anything at
+            reactjs/nextjs
+          </Heading>
+        </Flex>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+        <Grid templateColumns="repeat(5, 1fr)" gap={6} gridAutoFlow>
+          <Link
+            href="https://www.instagram.com/keremeesen"
+            isExternal
+            _hover={{ bg: "#3333" }}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            <Flex
+              w="100%"
+              h="100%"
+              justify="center"
+              align="center"
+              direction="column"
+            >
+              <Heading size="md">Instagram</Heading>
+              <FaInstagram />
+              <Text>keremeesen</Text>
+            </Flex>
+          </Link>
+          <Link
+            href="https://www.twitter.com/kerooxd"
+            isExternal
+            size="16px"
+            _hover={{ bg: "#3333" }}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+            <Flex
+              w="100%"
+              h="100%"
+              justify="center"
+              align="center"
+              direction="column"
+            >
+              <Heading size="md">Twitter</Heading>
+              <FaTwitter />
+              <Text>kerooxd</Text>
+            </Flex>
+          </Link>
+          <Link
+            href="https://www.github.com/keremesen"
+            isExternal
+            size="16px"
+            _hover={{ bg: "#3333" }}
+          >
+            <Box w="100%" h="20" textAlign="center">
+              <Flex
+                w="100%"
+                h="100%"
+                justify="center"
+                align="center"
+                direction="column"
+              >
+                <Heading size="md">GitHub</Heading>
+                <FaGithub />
+                <Text>keremesen</Text>
+              </Flex>
+            </Box>
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/kerem-esen-55a1941bb/"
+            isExternal
+            size="16px"
+            _hover={{ bg: "#3333" }}
+          >
+            <Flex
+              w="100%"
+              h="100%"
+              justify="center"
+              align="center"
+              direction="column"
+            >
+              <Heading size="md">LinkedIn</Heading>
+              <FaLinkedin />
+              <Text>keremesen</Text>
+            </Flex>
+          </Link>
+          <Link
+            href="mailto:kerem.esen.01@hotmail.com"
+            isExternal
+            _hover={{ bg: "#3333" }}
+          >
+            <Flex
+              w="100%"
+              h="100%"
+              justify="center"
+              align="center"
+              direction="column"
+            >
+              <Heading size="md">Mail</Heading>
+              <FaMailBulk />
+              <Text>kerem.esen.01@hotmail.com</Text>
+            </Flex>
+          </Link>
+        </Grid>
+      </Flex>
+    </Flex>
+  );
 }
