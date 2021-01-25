3d49a787e104d91bb28b0d3d12b9a205b6f874d5
Kerem Esen <kerem.esen.01@hotmail.com>
2021-01-25 21:20:30 +0300

    some updates

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,23 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    
-    <Flex align="center" justify="center"   h="100vh" bg="#111" w="100vw"  minW="100vw" minH="100vh"  >
+    <Flex
+      align="center"
+      justify="center"
+      h="100vh"
+      bg="#111"
+      w="100vw"
+      minW="100vw"
+      minH="100vh"
+    >
       <Head>
         <title>Kerem Esen</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        </Head> 
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
       <Flex
         direction="column"
         bg="#2222"
@@ -27,77 +37,60 @@ export default function Home() {
         borderRadius={15}
         h="60vh"
         minW="70%"
-        
-
       >
-        <Flex direction="column" m={8} minW="100%" >
-          <Heading>KEREM ESEN </Heading>
-          <Text fontSize="xl"> FULL-STACK DEVELOPER</Text>
+        <Flex direction="column" m={8} minW="100%">
+          <Heading letterSpacing={2}>KEREM ESEN </Heading>
+          <Text fontSize="xl" as="samp"> FULL-STACK DEVELOPER</Text>
         </Flex>
-        <Flex bg="#3333" h="30%" align="center" justify="center" my={5} >
-          <Heading size="md">
-            hi im kerem im 19 yo developer.im trying to learn and do something at
-            reactjs/nextjs
-          </Heading>
+        <Flex bg="#3333" h="25%" align="center" justify="center" my={5}>
+          <Text fontSize="xl" as="samp">
+            hi im kerem im 19 yo developer.im trying to learn and do something
+            at reactjs/nextjs
+          </Text>
         </Flex>
 
-        <Flex  w="100%" minW="100%"
-        align="center"
-        justify="space-around"
-        direction={["column","row"]}
-        minW="100%">
+        <Flex
+          w="100%"
+          minW="100%"
+          align="center"
+          justify="space-around"
+          direction={["column", "row"]}
+          minW="100%"
+        >
           <Link
             href="https://www.instagram.com/keremeesen"
             isExternal
-            _hover={{ bg: "#3333" }}
+            
           >
-            <Flex
-              w="100%"
-              h="100%"
-              justify="center"
-              align="center"
-              direction="column"
-            >
+            <Flex justify="center" align="center" direction="column">
               <Heading size="md">Instagram</Heading>
               <FaInstagram />
-              <Text>keremeesen</Text>
+              <Text as="samp">keremeesen</Text>
             </Flex>
           </Link>
           <Link
             href="https://www.twitter.com/kerooxd"
             isExternal
             size="16px"
-            _hover={{ bg: "#3333" }}
+            
           >
-            <Flex
-              w="100%"
-              h="100%"
-              justify="center"
-              align="center"
-              direction="column"
-            >
+            <Flex justify="center" align="center" direction="column">
               <Heading size="md">Twitter</Heading>
               <FaTwitter />
-              <Text>kerooxd</Text>
+              <Text as="samp">kerooxd</Text>
             </Flex>
           </Link>
           <Link
             href="https://www.github.com/keremesen"
             isExternal
             size="16px"
-            _hover={{ bg: "#3333" }}
+            
           >
-            <Box w="100%" h="20" textAlign="center">
-              <Flex
-                w="100%"
-                h="100%"
-                justify="center"
-                align="center"
-                direction="column"
-              >
+            <Box w="100%" h="100%" textAlign="center">
+              <Flex justify="center" align="center" direction="column">
                 <Heading size="md">GitHub</Heading>
                 <FaGithub />
-                <Text>keremesen</Text>
+                <Text as="samp">keremesen</Text>
               </Flex>
             </Box>
           </Link>
@@ -105,16 +98,10 @@ export default function Home() {
             href="https://www.linkedin.com/in/kerem-esen-55a1941bb/"
             isExternal
             size="16px"
-            _hover={{ bg: "#3333" }}
+            
           >
-            <Flex
-              w="100%"
-              h="100%"
-              justify="center"
-              align="center"
-              direction="column"
-            >
-              <Heading size="md">LinkedIn</Heading>
+            <Flex justify="center" align="center" direction="column">
+              <Heading size="md" as="samp">LinkedIn</Heading>
               <FaLinkedin />
               <Text>keremesen</Text>
             </Flex>
@@ -122,18 +109,12 @@ export default function Home() {
           <Link
             href="mailto:kerem.esen.01@hotmail.com"
             isExternal
-            _hover={{ bg: "#3333" }}
+
           >
-            <Flex
-              w="100%"
-              h="100%"
-              justify="center"
-              align="center"
-              direction="column"
-            >
+            <Flex justify="center" align="center" direction="column">
               <Heading size="md">Mail</Heading>
               <FaMailBulk />
-              <Text>kerem.esen.01@hotmail.com</Text>
+              <Text as="samp" fontSize="sm">kerem.esen.01@hotmail.com</Text>
             </Flex>
           </Link>
         </Flex>
