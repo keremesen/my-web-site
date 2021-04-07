f052c3045f71ab308870090a6092706285c82ec2
Kerem Esen <kerem.esen.01@hotmail.com>
2021-04-07 03:21:15 +0300

    add adsense

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,20 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-       <html lang="en">
-      <Head>
-        <title>Kerem Esen</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-      </Head>
+      <html lang="en">
+        <Head>
+          <title>Kerem Esen</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <script
+            data-ad-client="ca-pub-5502576488431149"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+        </Head>
       </html>
       <Component {...pageProps} />
     </ChakraProvider>
